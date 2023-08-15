c25b45bc7b11fc377054be74190110206efa1f57
RocioAlday <roo.alday@gmail.com>
2023-08-15 20:52:34 -0300

    prueba-deploy-db

--- api/src/db.js
+++ api/src/db.js
@@ -9,7 +9,7 @@ const bcrypt= require('bcrypt');
 
 //Acá iria la instancia de sequelize con los datos que tengo en env
 
-const sequelize = new Sequelize(DB_URL, {
+const sequelize = new Sequelize(`${DB_URL}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
